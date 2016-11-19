9a6906b67d17b883e4b5257838516eaa30d8bc5f
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-18 19:15:06 -0500

    fixed a big typo

--- lib/redditaccessor.rb
+++ lib/redditaccessor.rb
@@ -37,31 +37,27 @@ module Reddit_Accessor
 
     def access_api_post(url, params)
       HTTParty.post(url, 
-                  (url, 
                   :headers => {"Authorization" => "bearer #{@token}",
                                     'user-agent' => @user_agent },
                   :query => params
                   )
-                  )
+                  
     end
 
     def access_api_delete(url, params)
       HTTParty.delete(url, 
-                  (url, 
                   :headers => {"Authorization" => "bearer #{@token}",
                                     'user-agent' => @user_agent },
                   :query => params
                   )
-                  )
+                  
     end
 
     def access_api_put(url, params)
       HTTParty.put(url, 
-                  (url, 
                   :headers => {"Authorization" => "bearer #{@token}",
                                     'user-agent' => @user_agent },
                   :query => params
-                  )
                   )
     end
   end
